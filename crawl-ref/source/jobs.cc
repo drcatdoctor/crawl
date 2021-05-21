f9f793ccd006553f4de22b163ff58cf0f28a15d6
dr what <drwhat@badcrawler.org>
2021-05-21 15:53:22 -0400

    Revert "Don't allow selecting Jesters via initfile"
    This reverts commit d82a837c79dd88c7986a61c826dd1e4d3ddbe756.

--- crawl-ref/source/jobs.cc
+++ crawl-ref/source/jobs.cc
@@ -102,7 +102,7 @@ bool is_job_valid_choice(job_type job)
 {
     return is_valid_job(job)
 #if TAG_MAJOR_VERSION == 34
-        && job != JOB_STALKER && job != JOB_JESTER
+        && job != JOB_STALKER
 #endif
         ;
 }
